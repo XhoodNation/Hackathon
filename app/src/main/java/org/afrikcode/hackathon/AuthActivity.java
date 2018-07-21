e74817d05642d2b2689d33dc051b76e87b08657b
codemadeushappy <36009021+codemadeushappy@users.noreply.github.com>
2018-07-21 23:31:12 +0000

    Our Project for the facebook Hackathon 2018

--- app/src/main/java/org/afrikcode/hackathon/AuthActivity.java
+++ app/src/main/java/org/afrikcode/hackathon/AuthActivity.java
@@ -3,11 +3,24 @@ package org.afrikcode.hackathon;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.facebook.FacebookSdk;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class AuthActivity extends AppCompatActivity {
+
+    FirebaseAuth auth;
+    FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_auth);
+        auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();
+        if (user == null) {
+            setContentView(R.layout.activity_auth);
+            FacebookSdk.sdkInitialize(getApplicationContext());
+        }
+
     }
 }
--- app/src/main/java/org/afrikcode/hackathon/MainActivity.java
+++ app/src/main/java/org/afrikcode/hackathon/MainActivity.java
@@ -1,10 +1,18 @@
 package org.afrikcode.hackathon;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 
 import com.google.firebase.auth.FirebaseAuth;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
 
         //Init FirebaseAuth
         auth = FirebaseAuth.getInstance();
+
+        printKeyHash();
     }
 
     @Override
@@ -28,6 +38,22 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(MainActivity.this, AuthActivity.class);
             startActivity(intent);
             finish();
+        }
+    }
+
+    private void printKeyHash() {
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo("com.greatideasgh.nasana",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature:info.signatures){
+                MessageDigest md  = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash", Base64.encodeToString(md.digest(),Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
         }
     }
 }
