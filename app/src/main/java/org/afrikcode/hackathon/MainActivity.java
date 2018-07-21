deec6d2b0dc918d1d60afc7f0b6fb874b1730230
codemadeushappy <36009021+codemadeushappy@users.noreply.github.com>
2018-07-21 22:17:49 +0000

    Our Project for the facebook Hackathon 2018

--- app/src/main/java/org/afrikcode/hackathon/MainActivity.java
+++ app/src/main/java/org/afrikcode/hackathon/MainActivity.java
@@ -1,13 +1,33 @@
 package org.afrikcode.hackathon;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Init FirebaseAuth
+        auth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        //Check if user is not logged in
+        if (auth.getCurrentUser() == null){
+            Intent intent = new Intent(MainActivity.this, AuthActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
