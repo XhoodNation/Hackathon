7ef6ad4cb19ef2166847abeb5d47df0d511fc427
codemadeushappy <36009021+codemadeushappy@users.noreply.github.com>
2018-07-22 00:22:07 +0000

    Our Project for the facebook Hackathon 2018

--- app/src/main/java/org/afrikcode/hackathon/AuthActivity.java
+++ app/src/main/java/org/afrikcode/hackathon/AuthActivity.java
@@ -1,16 +1,37 @@
 package org.afrikcode.hackathon;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
 
 public class AuthActivity extends AppCompatActivity {
 
     FirebaseAuth auth;
     FirebaseUser user;
+
+    LoginButton loginButton;
+    CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +41,63 @@ public class AuthActivity extends AppCompatActivity {
         if (user == null) {
             setContentView(R.layout.activity_auth);
             FacebookSdk.sdkInitialize(getApplicationContext());
+
+            //Initializing Button
+            loginButton = (LoginButton) findViewById(R.id.login_button);
+
+            callbackManager = CallbackManager.Factory.create();
+            loginButton.setReadPermissions(Arrays.asList("email"));
+        }else {
+            Intent homeIntent=new Intent(AuthActivity.this,MainActivity.class);
+            startActivity(homeIntent);
+            finish();
         }
 
+    }
+    private void buttonclickLoginFb(View view){
+        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                handleFacebookToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                Toast.makeText(AuthActivity.this, "Process Cancelled", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Toast.makeText(AuthActivity.this, "Error Authenticating", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void updateUI(FirebaseUser myuserobj) {
+        Toast.makeText(this, myuserobj.getDisplayName().toString(), Toast.LENGTH_SHORT).show();
+    }
+
+    private void handleFacebookToken(AccessToken accessToken) {
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(accessToken.getToken());
+        auth.signInWithCredential(credential)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            FirebaseUser myuserobj = auth.getCurrentUser();
+                            updateUI(myuserobj);
+                        }else {
+                            Toast.makeText(AuthActivity.this,task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
